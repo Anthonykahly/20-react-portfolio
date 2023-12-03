45358ec4c96f8de117eaaca40474fb1ea6b56a40
A K <akahly@gmail.com>
2023-12-03 13:11:33 -0500

    navbar updating

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,32 +3,33 @@ import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-      <div>{/*insert logo */}</div>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#161A30] text-gray-300">
+      <div>{/* insert logo */}
+    </div>
 
-      <div>
-        <ul className="flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
+      {/* menu */}
 
-      {/*hamburger */}
-      <div className="hidden">
-        <FaBars />
-      </div>
-
-      {/*mobile menu*/}
-      <ul className="hidden">
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Work</li>
         <li>Contact</li>
       </ul>
 
-      {/*social media icons*/}
+      {/* hamburger */}
+      <div className="md:hidden">
+        <FaBars />
+      </div>
+
+      {/* mobile menu */}
+      <ul className="absolute top-0 left-0 w-full h-screen bg-[#161A30] flex flex-col justify-center items-center">
+        <li>Home</li>
+        <li>About</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* social media icons */}
       <div className="hidden"></div>
     </div>
   );
