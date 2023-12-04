bac66f989a03b0f2714b25e083fc7593d573b708
A K <akahly@gmail.com>
2023-12-04 13:28:39 -0500

    homepage start

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,15 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Hero = () => {
   return (
-    <div className="w-full h-screen bg-[#31304D]">
+    <div name="hero" className="w-full h-screen bg-[#31304D]">
+
+        {/* Container */}
+        <div className="max-w-[1000px] mx-auto px-9 flex flex-col justify-center h-full">
+            <p>Hi, my name is</p>
+            <h1>Anthony Kahly</h1>
+            <h2>I am a full stack developer</h2>
+            <p></p>
+         </div>
 
     </div>
   )
