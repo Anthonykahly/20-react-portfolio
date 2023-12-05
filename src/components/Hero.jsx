353786da1d5d840126811aed1a84e9242fe654a4
A K <akahly@gmail.com>
2023-12-05 17:00:42 -0500

    portfolio added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,14 @@ const Hero = () => {
     <div name="home" className="w-full h-screen bg-[#B6BBC4]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-[#31304D]">Hi, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#161A30]">
+        <p className="text-[#31304D] font-bold">Hi, my name is</p>
+        <h1 className="text-5xl sm:text-7xl font-extrabold text-[#161A30]">
           Anthony Kahly
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#31304D]">
           Full Stack Web Developer.
         </h2>
-        <p className="text-[#4f4d81] py-4 max-w-[700px]">
+        <p className="text-[#4f4d81] py-4 max-w-[700px] font-bold">
           I am a recent graduate of the 2023 CWRU full stack course. 
         </p>
         <div>
